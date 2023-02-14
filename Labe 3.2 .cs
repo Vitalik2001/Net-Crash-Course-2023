7fc1d82f90eda4ab498904df18c9796e11908294
Vitalik2001 <123649726+Vitalik2001@users.noreply.github.com>
2023-02-14 16:17:23 +0200

    Add files via upload

---
+++ Labe 3.2 .cs
@@ -0,0 +1,35 @@
+﻿namespace Labe_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Job 2 --------------------");
+            //string a = Console.ReadLine()!;
+            Random random = new Random();
+            int b = random.Next(1, 10);
+            int counter = 0;
+            while (true)
+            {
+                Console.Write("Guess the number: ");
+                int guess = int.Parse(Console.ReadLine());
+                counter++;
+
+                if (guess == b)
+                {
+                    Console.WriteLine("You got it! The number was {0}.", b);
+                    Console.WriteLine("It took you {0} attempts to guess the number.", counter);
+                    break;
+                }
+                else if (guess < b)
+                {
+                    Console.WriteLine("The number is higher.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is lower.");
+                }
+            }
+        }
+    }
+}
---
+++ Labe 3.3.cs
@@ -0,0 +1,35 @@
+﻿namespace Labe_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Job 3 --------------------");
+            Random random1 = new Random();
+            const int A = 20;
+
+            var randomMassiv = new int[A];
+            for (int i = 0; i < A; i++)
+            {
+
+                randomMassiv[i] = random1.Next(1, 100);
+
+
+            }
+            Console.WriteLine("randomMassiv with random numbers:");
+            foreach (int num in randomMassiv)
+            {
+                Console.Write(num + " ");
+            }
+
+
+            int[] numbers = new int[20];
+
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+            }
+        }
+    }
+}
