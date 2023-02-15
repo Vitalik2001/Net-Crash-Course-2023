85cc9a2d4a806ceb896cae394d47d39a0197f79b
Vitalik2001 <123649726+Vitalik2001@users.noreply.github.com>
2023-02-15 12:03:29 +0200

    Add files via upload

---
+++ Labe 3.1 .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Transactions;
+
+namespace Net_Crahs_2023_labe_3__Array
+{
+    internal class Program
+    {
+        //enum Rand { amount = 1, sort , even_values, Max}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a given");
+            char given = Console.ReadLine()[0];
+
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Output ");
+            foreach (int i in Enumerable.Range(0, number))
+            {
+                Console.WriteLine(given);
+            }
+
+
+        }
+    }
+}
